caa9d18eac602dc327488db54d45f0c2609eb7e7
Victor Makoed <vmakoed@gmail.com>
2019-07-21 14:53:11 +0300

    Disable database

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,11 +38,11 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
-
-  # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
+  # # Raise an error on page load if there are pending migrations.
+  # config.active_record.migration_error = :page_load
+  #
+  # # Highlight code that triggered database queries in logs.
+  # config.active_record.verbose_query_logs = true
 
 
   # Raises error for missing translations
